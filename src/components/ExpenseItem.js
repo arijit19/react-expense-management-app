87c1d56d969e034bd0432bd8b3aeccb15ff4d859
arijitchandra <arijit.chandra03@gmail.com>
2020-06-13 00:42:30 +0530

    change in amount format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import moment from 'moment';
+import numeral from 'numeral';
 import { Link } from 'react-router-dom';
 
 const ExpenseItem = ({ id, description, amount, createdAt})=> (
     <div>
         <h3><Link to={`/edit/:${id}`}>Description</Link> : {description}</h3>
-        <p>Amount       : {amount}</p>
+        <p>Amount       : {numeral(amount).format('$0,0.0000')}</p>
         <p>CreateAt     : {moment(createdAt).format('Do MMM YYYY')}</p>
     </div>
 );
